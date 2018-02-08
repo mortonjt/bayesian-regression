e5ffe0b86b3d3f1f541af40035bd6feb91ad412e
mortonjt <jamietmorton@gmail.com>
2018-02-08 12:27:33 -0800

    ENH: Updating launch script to work with torque

--- bayesian_regression/benchmarks/effect_size.py
+++ bayesian_regression/benchmarks/effect_size.py
@@ -2,24 +2,25 @@ import os
 import shutil
 import subprocess
 import numpy as np
-from snakemake import snakemake
 from bayesian_regression.util.generators import band_table, block_table, deposit
 import yaml
 
 # snakemake config
 config_file = 'config.yaml'
 workflow_type = 'local'
-local_cores = 1
-jobs = 1
+local_cores = 2
+jobs = 2
 force = False
-snakefile = '../Snakefile'
+snakefile = '../../Snakefile'
 dry_run = True
-output_dir = 'benchmark_results/'
+output_dir = 'effect_size_results/'
 quiet=True
 keep_logger=True
+workflow_type = 'torque'
+cluster_config = 'cluster.json'
 
 # simulation parameters
-regenerate_simulations = True
+regenerate_simulations = False
 num_samples = 50
 num_features = 200
 #mu_num = 8            # mean of the numerator taxa
@@ -27,7 +28,7 @@ mu_null = 0            # mean of the common taxa
 #mu_denom = 2          # mean of the denominator taxa
 max_diff = 4           # largest separation between the normals
 min_diff = 0.05        # smallest separation between the normals
-sigma = 1              # variance of the random effects distribution
+sigma = 0.5            # variance of the random effects distribution
 pi1 = 0.1              # percentage of the species
 pi2 = 0.3              # percentage of the species
 low = -4               # lower value for spectrum
@@ -77,13 +78,33 @@ if regenerate_simulations:
             metadata[benchmark] = ef
             deposit(table, metadata, feature_metadata, i, r, output_dir)
 
+if workflow_type == 'local':
+    cmd = ' '.join([
+        'snakemake --verbose',
+        '--snakefile %s ' % snakefile,
+        '--local-cores %s ' % local_cores,
+        '--jobs %s ' % jobs,
+        '--configfile %s ' % config_file,
+    ])
 
-cmd = ' '.join([
-    'snakemake ',
-    '--snakefile %s ' % snakefile,
-    '--local-cores %s ' % local_cores,
-    '--jobs %s ' % jobs,
-    '--configfile %s ' % config_file,
-])
+elif workflow_type == 'torque':
+    eo = '-e {cluster.error} -o {cluster.output} '
+
+    cluster_setup = '\"qsub %s\
+                     -l nodes=1:ppn={cluster.n} \
+                     -l mem={cluster.mem}gb \
+                     -l walltime={cluster.time}\" ' % eo
+
+    cmd = ' '.join(['snakemake --verbose',
+                    '--snakefile %s ' % snakefile,
+                    '--local-cores %s ' % local_cores,
+                    '--jobs %s ' % jobs,
+                    '--cluster-config %s ' % cluster_config,
+                    '--cluster %s '  % cluster_setup,
+                    '--configfile %s ' % config_file
+                ])    
+else:
+    ValueError('Incorrect workflow specified:', workflow_type)
+
 proc = subprocess.Popen(cmd, shell=True)
 proc.wait()
